3a6b197456da81fb7cfc911beb4764e48d0e31fd
Luper Rouch <luper.rouch@gmail.com>
2016-01-14 18:23:56 +0100

    pep8 fixes

--- robgracli/http.py
+++ robgracli/http.py
@@ -21,28 +21,34 @@ class HttpClient(object):
     '''
 
     def __init__(self, connect_timeout=5, read_timeout=2.5, max_retries=3,
-            backoff_factor=1, **extra_requests_opts):
+                 backoff_factor=1, **extra_requests_opts):
         self.timeout = (connect_timeout, read_timeout)
         self.extra_requests_opts = extra_requests_opts
         self.session = requests.Session()
-        self.session.mount('http://', get_adapter(max_retries, backoff_factor))
-        self.session.mount('https://', get_adapter(max_retries, backoff_factor))
+        self.session.mount('http://',
+                           get_adapter(max_retries, backoff_factor))
+        self.session.mount('https://',
+                           get_adapter(max_retries, backoff_factor))
 
     def request(self, method, url, data=None, params=None,
-            raise_for_status=True):
-        response = self.session.request(method, url, data=data, params=params,
-                timeout=self.timeout, **self.extra_requests_opts)
+                raise_for_status=True):
+        response = self.session.request(method,
+                                        url,
+                                        data=data,
+                                        params=params,
+                                        timeout=self.timeout,
+                                        **self.extra_requests_opts)
         if raise_for_status:
             response.raise_for_status()
         return response
 
     def get(self, url, params=None, raise_for_status=True):
         return self.request('GET', url, data=None, params=params,
-                raise_for_status=raise_for_status)
+                            raise_for_status=raise_for_status)
 
     def post(self, url, data=None, params=None, raise_for_status=True):
         return self.request('POST', url, data=data, params=params,
-                raise_for_status=raise_for_status)
+                            raise_for_status=raise_for_status)
 
 
 def get_adapter(max_retries, backoff_factor):
